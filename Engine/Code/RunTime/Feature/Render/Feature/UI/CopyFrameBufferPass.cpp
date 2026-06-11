21b050fb42190b3960e2917003f20bb7fa4740fd
KK-5 <761806790@qq.com>
2026-06-11 18:35:51 +0800

    CopyFrameBufferPass: clear swapchain to black before copy
    Barrier swapchain to RENDER_TARGET briefly for ClearRenderTarget,
    then back to COPY_DEST for the framebuffer blit. Avoids stale
    contents bleeding through transparent UI regions.

--- Engine/Code/RunTime/Feature/Render/Feature/UI/CopyFrameBufferPass.cpp
+++ Engine/Code/RunTime/Feature/Render/Feature/UI/CopyFrameBufferPass.cpp
@@ -35,6 +35,8 @@ namespace Spark::Render
                 outputBind.m_usage  = RHI::AttachmentUsage::Copy;
                 outputBind.m_stage  = RHI::AttachmentStage::Copy;
                 outputBind.m_access = RHI::AttachmentAccess::Write;
+                outputBind.m_action.m_loadAction = RHI::AttachmentLoadAction::Clear;
+                outputBind.m_action.m_storeAction = RHI::AttachmentStoreAction::Store;
                 outputBind.m_view   = builder.GetCurrentSwapChainView();
 
                 builder.ImportImageAttachment<SPARK_PASS_TAG("CopyFrameBufferPass")>(
@@ -50,6 +52,44 @@ namespace Spark::Render
                 {
                     return;
                 }
+
+                // ExecutePreBarriers has already transitioned the swapchain to COPY_DEST.
+                // Push it to RENDER_TARGET temporarily so ClearRenderTarget can run,
+                // then return it to COPY_DEST for the copy below.
+                RHI::ImageBarrier toRT;
+                toRT.m_image     = copyDst;
+                toRT.m_srcUsage  = RHI::AttachmentUsage::Copy;
+                toRT.m_dstUsage  = RHI::AttachmentUsage::RenderTarget;
+                toRT.m_srcAccess = RHI::AttachmentAccess::Write;
+                toRT.m_dstAccess = RHI::AttachmentAccess::Write;
+                toRT.m_srcStage  = RHI::AttachmentStage::Copy;
+                toRT.m_dstStage  = RHI::AttachmentStage::ColorAttachmentOutput;
+                toRT.m_srcQueue  = RHI::HardwareQueueClass::Graphics;
+                toRT.m_dstQueue  = RHI::HardwareQueueClass::Graphics;
+                work.m_commandList->QueueBarrier(toRT);
+                work.m_commandList->FlushBarriers();
+
+                auto* swapChainView = FindPassAttachmentImageView<SPARK_PASS_TAG("CopyFrameBufferPass")>(rhiCtx, RHI::InputName("CopyWrite"));
+                if (swapChainView)
+                {
+                    RHI::ImageClearRequest clearReq;
+                    clearReq.m_clearValue = RHI::ClearValue::CreateVector4Float(0.f, 0.f, 0.f, 1.f);
+                    clearReq.m_imageView = swapChainView;
+                    work.m_commandList->ClearRenderTarget(clearReq);
+                }
+
+                RHI::ImageBarrier toCopy;
+                toCopy.m_image     = copyDst;
+                toCopy.m_srcUsage  = RHI::AttachmentUsage::RenderTarget;
+                toCopy.m_dstUsage  = RHI::AttachmentUsage::Copy;
+                toCopy.m_srcAccess = RHI::AttachmentAccess::Write;
+                toCopy.m_dstAccess = RHI::AttachmentAccess::Write;
+                toCopy.m_srcStage  = RHI::AttachmentStage::ColorAttachmentOutput;
+                toCopy.m_dstStage  = RHI::AttachmentStage::Copy;
+                toCopy.m_srcQueue  = RHI::HardwareQueueClass::Graphics;
+                toCopy.m_dstQueue  = RHI::HardwareQueueClass::Graphics;
+                work.m_commandList->QueueBarrier(toCopy);
+                work.m_commandList->FlushBarriers();
 
                 RHI::Origin dstOrigin{};
                 const auto* ui = Service<UI::UIBaseSystem>::Get();
